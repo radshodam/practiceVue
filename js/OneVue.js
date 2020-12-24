247e82a1f03ffa427af5fba76047786094f5cee4
mohamadZareai <mohamadzareai@gmail.com>
2020-12-25 00:59:10 +0330

    add vue practice+

---
+++ js/OneVue.js
@@ -0,0 +1,26 @@
+new Vue({
+  el: '#app',
+  data: {
+      name:'mohamad zareai'
+  }
+});
+
+// lesson one VUE JS
+
+/*darse aval
+01 new vue 
+inke ba sakhtan yek new vue mitoonam yek
+ya kol eleman haii khodam ra pas bedam*/
+
+/* 02 el:
+ba moarefi mikham be noghteie mored nazar dar html 
+dastrsi peid konam mesl gps*/
+
+/*03 data:{}
+ba sakht object data ke daronesh por mishavad az etelati ke man mikham pas
+bedam be oun id ke daroonesh yek seri etelatesh ro mikham az mane DATA begire
+*/
+
+// 04 marhale chaharom miam be ghesmat html va ba {{X}} pas midamesh 
+
+//{{name jaigozin shod}}
